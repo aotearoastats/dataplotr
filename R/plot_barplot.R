5ebf644fe1674b3029eb4a6c6742e4d439fdabe7
Alexander Kegel <afk@mailbox.org>
2017-09-04 10:36:48 +1200

    Adust sizes, improve documentation

--- R/plot_barplot.R
+++ R/plot_barplot.R
@@ -12,7 +12,7 @@ plot_barplot <- function(data_frame,
                          y_facet = FALSE,
                          label_vars = FALSE,
                          percentage = FALSE,
-                         color_vars = NULL, fill_vars = NULL,
+                         color_vars = NULL, fill_vars = color_vars,
                          colour_palette = NULL, fill_palette = NULL,
                          pos = "dodge",
                          barplot_alpha = 1,
@@ -27,47 +27,42 @@ plot_barplot <- function(data_frame,
                          basesize = 12,
                          flip = FALSE){
   #' Draws barplots.
-  #'
+  #' 
   #' @param data_frame: A data frame
-  #' @param main_title: Title of the barplot, defaults to NULL.
-  #' @param bin_width: Width of the bars, defaults to NULL.
-  #' @param text_col: Text colour, defaults to black_seq[8]
-  #' @param basesize: Text basesize, defaults to 12
-  #' @param flip: Flips x-axis and y-axis if TRUE, defaults to FALSE
-  #'
-  #' @param x_title: X-axis title, string in quotations, e.g., "Year Level"
-  #' @param x_var: X-axis variable, has to be a factor
-  #' @param x_limits: Scale limits, e.g., c(4,10) for a year level variable
-  #' @param x_breaks: Breaks, e.g., c(4, 6, 9) for a year level variable
-  #'
-  #' @param y_title: Y-axis title, string in quotations, e.g., "Percentage of 
-  #'   Blocks"
-  #' @param y_var: Y-axis variable
-  #' @param y_limits: Scale limits, e.g., c(0, 1) for a percentage variable
-  #' @param y_breaks: Breaks, e.g., c(0, 0.5, 1) for a percentage variable
-  #'
-  #' @param y_facet: A grid for the y-variable controlled via a formula in 
-  #'   quotations e.g., "SCHOOL ~ ." to have the y-variable sliced horizontally 
+  #' @param main_title: A string, optional main title of the barplot
+  #' @param bar_width: A floating number, optional bar width
+  #'   
+  #' @param x_var: A factor
+  #' @param x_title: A string, optional title of the x-axis
+  #' @param x_limits: A vector of length 2, optional scale limits, e.g., c(4,10)
+  #' @param x_breaks: A vector, optional scale breaks, e.g., c(4, 6, 9)
+  #'   
+  #' @param y_var: A numerical variable
+  #' @param y_title: A string, optional title of the y-axis
+  #' @param y_limits: A vector of length 2, optional scale limits, e.g., c(0, 1)
+  #' @param y_breaks: A vector, optional breaks, e.g., c(0.0, 0.5, 1.0)
+  #'   
+  #' @param y_facet: A formular, an optional grid for the y-variable controlled
+  #'   by y_facet e.g., "SCHOOL ~ ." to have the y-variable sliced horizontally 
   #'   by a school name variable, or ". ~ SCHOOL" to have the y-variable sliced 
   #'   vertically by a school name variable. Defaults to FALSE.
-  #' @param label_vars: A variable for bar labels, e.g, 
-  #'   label_vars="percent(FREQ)"
-  #' @param percentage: whether the quantitative variable should have percentage
-  #'   labels
+  #' @param label_vars: An optional label for the bars , e.g, "percent(FREQ)"
+  #' @param percentage: logical, if TRUE assumes that y_var is a percentage
+  #'
+  #' @param fill_vars: A factor, an optional variable by which to slice the
+  #'   x-variable, used for filling the bars
+  #' @param fill_palette: A vector of length equal to the number of categories
+  #'   of fill_vars, an optional colour palette, defaults to fill_brewer
+  #' @param color_vars: A factor, an optional variable by which to slice the
+  #'   x-variable, usually equal to fill_vars, used for colouring the outside
+  #'   edge of the bars
+  #' @param colour_palette: A vector of length equal to the number of categories
+  #'   of color_vars, an optional colour palette, usually equal to fill_palette,
+  #'   but can be used to highlight the bar borders, defaults to colour_brewer
   #'   
-  #' @param color_vars: variable by which to cross the x-variable, used for 
-  #'   colouring the bars, has to be factor
-  #' @param fill_vars: variable by which to cross the x-variable, used for 
-  #'   filling the bars, has to be factor
-  #' @param colour_palette: palette values for scale_colour, defaults to 
-  #'   colour_brewer
-  #' @param fill_palette: palette values for scale_fill, defaults to fill_brewer
+  #' @param pos: A position object, an optional placement rule for the bars, defaults to "dodge"
+  #' @param barplot_alpha: An floating number between 0 and 1, an optional degree of transparency for the bars, default at 1
   #'   
-  #' @param pos: position of boxplot, default at "dodge"
-  #' @param barplot_alpha: degree of transparency of barplot, default at 1
-  #'   
-  #'   
-  #'   legend arguments
   #' @param colour_title: legend guide for scale_colour, setting a title 
   #'   activates the legend, defaults to FALSE.
   #' @param fill_title: legend guide for scale_fill, setting a title activates 
@@ -81,6 +76,9 @@ plot_barplot <- function(data_frame,
   #'   be reversed
   #' @param legend_col: legend background colour, default at "#E5E2E0" keysize: 
   #'   size of the legend
+  #' @param text_col: Text colour, defaults to black_seq[8]
+  #' @param basesize: Text basesize, defaults to 12
+  #' @param flip: Flips x-axis and y-axis if TRUE, defaults to FALSE
   #'   
   #' @return a bar plot object
   #' @examples
@@ -200,11 +198,11 @@ plot_barplot <- function(data_frame,
                           ## legend.justification = c(1, 1),
                           legend.position = legend_pos,
                           axis.text.x = ggplot2::element_text(size = basesize, colour = text_col, lineheight = 1.2, vjust = 1),
-                          axis.text.y = ggplot2::element_text(size = basesize-2, colour = text_col, lineheight = 1.2, vjust = 0.5),
+                          axis.text.y = ggplot2::element_text(size = basesize, colour = text_col, lineheight = 1.2, vjust = 0.5),
                           axis.title.x = ggplot2::element_text(vjust = 3.1804, colour = text_col, size = basesize + 2, margin=ggplot2::margin(25,0,0,0)),
                           axis.title.y = ggplot2::element_text(vjust = 0.6316, colour = text_col, size = basesize + 2),
                           axis.ticks.length = ggplot2::unit(.20888, "cm"),
-                          plot.title = ggplot2::element_text(vjust = 3.8746, hjust = 0, colour = text_col, size = basesize + 8),
+                          ## plot.title = ggplot2::element_text(vjust = 3.8746, hjust = 0, colour = text_col, size = basesize + 8),
                           plot.margin = ggplot2::unit(c(.8835, .582083, 0.0748, .9378), "cm"),
                           strip.background = ggplot2::element_rect(fill = "#ececec", colour = black_seq[8]), # facet background
                           strip.text.y = ggplot2::element_text(size = 8, angle = 0)) # for facet text
@@ -212,7 +210,7 @@ plot_barplot <- function(data_frame,
   ## title
   if(exists("main_title")){
     ## p <- p + ggplot2::ggtitle(main_title)
-    title <- cowplot::ggdraw() + cowplot::draw_label(main_title) ##, fontface='bold')
+    title <- cowplot::ggdraw() + cowplot::draw_label(main_title, size=basesize+5) ##, fontface='bold')
     p <- cowplot::plot_grid(title, p, ncol=1, rel_heights=c(0.1, 1))
   }
   return(p)
