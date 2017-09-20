ac5265abecb6d92387c861e6d17616139f714d35
Alexander Kegel <afk@mailbox.org>
2017-09-20 16:47:50 +1200

    Reformat for readability

--- R/plot_barplot.R
+++ R/plot_barplot.R
@@ -122,108 +122,116 @@ plot_barplot <-
     #' 
     #' @section Bugs: Guide colour and guide fill currently take the same 
     #'   arguments and can not be controlled individually.
-    #'   
+    #' 
     #' @export
-    
+
     set_bar_theme()
-    
+
     p <- ggplot2::ggplot(data = data_frame)
-    
+
     p <- p + ggplot2::aes_string(
-      x = x_var,
-      y = y_var,
-      fill = fill_vars,
-      colour = colour_vars,
-      label = label_vars
-    )
-    
+                        x = x_var,
+                        y = y_var,
+                        fill = fill_vars,
+                        colour = colour_vars,
+                        label = label_vars
+                      )
+
     p <- p + ggplot2::geom_bar(
-      position = pos,
-      alpha = barplot_alpha,
-      stat = "identity",
-      width = bar_width,
-      size = 1
-    ) ## controls border thickness
-    
+                        position = pos,
+                        alpha = barplot_alpha,
+                        stat = "identity",
+                        width = bar_width,
+                        size = 1
+                      ) ## controls border thickness
+
     p <- p + ggplot2::scale_x_discrete(
-      x_title,
-      drop = FALSE,
-      limits = rev(x_limits),
-      breaks = x_breaks
-    )
-    
-    
+                        x_title,
+                        drop = FALSE,
+                        limits = rev(x_limits),
+                        breaks = x_breaks
+                      )
+
+
     ## percentage for y scale
     if (percentage == TRUE) {
       p <- p + ggplot2::scale_y_continuous(
-        y_title,
-        labels = scales::percent,
-        limits = y_limits,
-        breaks = y_breaks
-      )
+                          y_title,
+                          labels = scales::percent,
+                          limits = y_limits,
+                          breaks = y_breaks
+                        )
     } else {
-      p <- p + ggplot2::scale_y_continuous(y_title,
-                                           limits = y_limits,
-                                           breaks = y_breaks)
+      p <- p + ggplot2::scale_y_continuous(
+                          y_title,
+                          limits = y_limits,
+                          breaks = y_breaks)
     }
-    
+
     ## colour either manual or brewer
     if (!(is.null(colour_palette)) & !(is.null(fill_palette))) {
-      p <- p + ggplot2::scale_colour_manual(values = colour_palette,
-                                            labels = legend_labels)
-      p <- p + ggplot2::scale_fill_manual(values = fill_palette,
-                                          labels = legend_labels)
+      p <- p + ggplot2::scale_colour_manual(
+                          values = colour_palette,
+                          labels = legend_labels)
+      p <- p + ggplot2::scale_fill_manual(
+                          values = fill_palette,
+                          labels = legend_labels)
     } else {
-      p <- p + ggplot2::scale_colour_brewer(labels = legend_labels,
-                                            direction = -1)
-      p <- p + ggplot2::scale_fill_brewer(labels = legend_labels,
-                                          direction = -1)
+      p <- p + ggplot2::scale_colour_brewer(
+                          labels = legend_labels,
+                          direction = -1)
+      p <- p + ggplot2::scale_fill_brewer(
+                          labels = legend_labels,
+                          direction = -1)
     }
-    
+
     ## facet
     if (!(y_facet == FALSE)) {
       p <-
         p + ggplot2::facet_grid(y_facet, drop = TRUE, scales = "free_x")
     }
-    
+
     ## legend
     p <- p + ggplot2::guides(colour = "none",
                              fill = "none")
-    
+
     if (!(colour_title == FALSE)) {
       p <-
-        p + ggplot2::guides(colour = ggplot2::guide_legend(title = colour_title, reverse = legend_rev))
+        p + ggplot2::guides(colour = ggplot2::guide_legend(
+                                                title = colour_title,
+                                                reverse = legend_rev))
     }
-    
+
     if (!(fill_title == FALSE)) {
       p <-
-        p + ggplot2::guides(fill = ggplot2::guide_legend(title = fill_title, reverse = legend_rev))
+        p + ggplot2::guides(fill = ggplot2::guide_legend(
+                                              title = fill_title,
+                                              reverse = legend_rev))
     }
-    
+
     ## flip axes
     if (flip) {
       p <- p + ggplot2::coord_flip()
     }
-    
+
     ## bar labels
     if (!(label_vars == FALSE)) {
       p <-
         p + ggplot2::geom_label(
-          ggplot2::aes_string(y = paste(y_var, " - 0.00"), fill = rev(fill_vars)),
-          colour = ggplot2::theme_get()[["bar.label.color"]],
-          size = ggplot2::theme_get()[["bar.label.size"]],
-          show.legend = FALSE,
-          position = pos
-        )
+                       ggplot2::aes_string(y = paste(y_var, " - 0.00"), fill = rev(fill_vars)),
+                       colour = ggplot2::theme_get()[["bar.label.color"]],
+                       size = ggplot2::theme_get()[["bar.label.size"]],
+                       show.legend = FALSE,
+                       position = pos
+                     )
     }
-    
+
     ## x_ticks switches off labels and ticks on the x-axis
     if (!x_ticks) {
       p <- p + ggplot2::theme(axis.text.x = element_blank(),
                               axis.ticks.x = element_blank())
     }
-    
-    
+
     ## title
     if (!is.null(main_title)) {
       cow <- cowplot::ggdraw()
@@ -232,6 +240,6 @@ plot_barplot <-
       p <-
         cowplot::plot_grid(cow, p, ncol = 1, rel_heights = c(0.1, 1))
     }
-    
+
     return(p)
   }
