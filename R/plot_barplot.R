b00e57e5ed413abfcd2c64528c2eb7c1e6cd1b92
Alexander Kegel <afk@mailbox.org>
2017-12-11 14:28:32 +1300

    Set theme onLoad

--- R/plot_barplot.R
+++ R/plot_barplot.R
@@ -1,3 +1,101 @@
+# @include set_bar_theme
+
+#' Draws barplots.
+#'
+#' @param data_frame A data frame
+#'
+#' @param x_var A factor
+#' @param y_var A numerical variable
+#'
+#' @param main_title A string, optional main title of the barplot
+#' @param bar_width A floating number, optional bar width
+#'
+#' @param x_title A string, optional title of the x-axis
+#' @param x_limits A vector of length 2, optional scale limits, e.g.,
+#'   c(4,10)
+#' @param x_breaks A vector, optional scale breaks, e.g., c(4, 6, 9)
+#' @param x_ticks Logical, FALSE switches off tick marks on the x-axis
+#'
+#' @param y_title A string, optional title of the y-axis
+#' @param y_limits A vector of length 2, optional scale limits, e.g., c(0,
+#'   1)
+#' @param y_breaks A vector, optional breaks, e.g., c(0.0, 0.5, 1.0)
+#'
+#' @param y_facet A formular, an optional grid for the y-variable
+#'   controlled by y_facet e.g., "SCHOOL ~ ." to have the y-variable sliced
+#'   horizontally by a school name variable, or ". ~ SCHOOL" to have the
+#'   y-variable sliced vertically by a school name variable. Defaults to
+#'   FALSE.
+#' @param label_vars An optional label for the bars , e.g, "percent(FREQ)"
+#' @param percentage logical, if TRUE assumes that y_var is a percentage
+#'
+#' @param fill_vars A factor, an optional variable by which to slice the
+#'   x-variable, used for filling the bars
+#' @param fill_palette A vector of length equal to the number of categories
+#'   of fill_vars, an optional colour palette, defaults to fill_brewer
+#' @param colour_vars A factor, an optional variable by which to slice the
+#'   x-variable, usually equal to fill_vars, used for colouring the outside
+#'   edge of the bars
+#' @param colour_palette A vector of length equal to the number of
+#'   categories of colour_vars, an optional colour palette, usually equal to
+#'   fill_palette, but can be used to highlight the bar borders, defaults to
+#'   colour_breweR
+#'
+#' @param pos A position, an optional placement rule for the bars, defaults
+#'   to "dodge"
+#' @param barplot_alpha A floating number between 0 and 1, an optional
+#'   degree of transparency for the bars, default at 1
+#'
+#' @param colour_title legend guide for scale_colour, setting a title
+#'   activates the legend, requires colour_vars to be set, defaults to
+#'   FALSE.
+#' @param fill_title legend guide for scale_fill, setting a title activates
+#'   the legend, requires fill_vars to be set, default at FALSE.
+#'
+#' @param legend_labels a vector of labels for the legend, e.g,
+#'   c("M\\u0101ori", "Pakeh\\u0101")
+#' @param legend_rev determines whether the order of the legend labels
+#'   should be reversed
+#' @param flip Flips x-axis and y-axis if TRUE, defaults to FALSE
+#'
+#' @return a bar plot object
+#' @examples
+#'
+#' \dontrun{
+#' library(tidyverse)
+#' iris.plot <- iris %>% group_by(Species) %>% summarize(Sepal.Length=mean(Sepal.Length))
+#'
+#'   #Simple bar plot
+#'   p <- plot_barplot(iris.plot,
+#'                     x_var = "Species",
+#'                     y_var = "Sepal.Length")
+#'   plot(p)
+#'
+#'   #Same bar plot with fill by Species and border colour by Species reversed
+#'   p <- plot_barplot(iris.plot,
+#'                     main_title="Sepal.Length by Species on the Iris Data Set",
+#'                     x_var = "Species",
+#'                     y_var = "Sepal.Length",
+#'                     fill_title = "Species",
+#'                     fill_vars = "Species",
+#'                     colour_vars = "rev(Species)")
+#'   plot(p)
+#'
+#'   #Same bar plot with a facet for Sepal.Width and a legend with a title
+#'   p <- plot_barplot(iris,
+#'                     x_var = "Species",
+#'                     y_var = "Sepal.Length",
+#'                     fill_vars = "Species",
+#'                     y_facet = "Sepal.Width ~ .",
+#'                     fill_title = "Species",
+#'                     flip = TRUE)
+#'   plot(p)}
+#'
+#' @section Bugs:
+#' Guide colour and guide fill currently take the same arguments and can
+#' not be controlled individually.
+#'
+#' @export
 plot_barplot <-
   function(data_frame,
            x_var,
@@ -25,103 +123,6 @@ plot_barplot <-
            legend_labels = ggplot2::waiver(),
            legend_rev = FALSE,
            flip = FALSE){
-    #' Draws barplots.
-    #' 
-    #' @param data_frame A data frame
-    #'   
-    #' @param x_var A factor
-    #' @param y_var A numerical variable
-    #'   
-    #' @param main_title A string, optional main title of the barplot
-    #' @param bar_width A floating number, optional bar width
-    #'   
-    #' @param x_title A string, optional title of the x-axis
-    #' @param x_limits A vector of length 2, optional scale limits, e.g.,
-    #'   c(4,10)
-    #' @param x_breaks A vector, optional scale breaks, e.g., c(4, 6, 9)
-    #' @param x_ticks Logical, FALSE switches off tick marks on the x-axis
-    #'   
-    #' @param y_title A string, optional title of the y-axis
-    #' @param y_limits A vector of length 2, optional scale limits, e.g., c(0,
-    #'   1)
-    #' @param y_breaks A vector, optional breaks, e.g., c(0.0, 0.5, 1.0)
-    #'   
-    #' @param y_facet A formular, an optional grid for the y-variable
-    #'   controlled by y_facet e.g., "SCHOOL ~ ." to have the y-variable sliced
-    #'   horizontally by a school name variable, or ". ~ SCHOOL" to have the
-    #'   y-variable sliced vertically by a school name variable. Defaults to
-    #'   FALSE.
-    #' @param label_vars An optional label for the bars , e.g, "percent(FREQ)"
-    #' @param percentage logical, if TRUE assumes that y_var is a percentage
-    #'
-    #' @param fill_vars A factor, an optional variable by which to slice the 
-    #'   x-variable, used for filling the bars
-    #' @param fill_palette A vector of length equal to the number of categories
-    #'   of fill_vars, an optional colour palette, defaults to fill_brewer
-    #' @param colour_vars A factor, an optional variable by which to slice the 
-    #'   x-variable, usually equal to fill_vars, used for colouring the outside 
-    #'   edge of the bars
-    #' @param colour_palette A vector of length equal to the number of
-    #'   categories of colour_vars, an optional colour palette, usually equal to
-    #'   fill_palette, but can be used to highlight the bar borders, defaults to
-    #'   colour_brewer
-    #'
-    #' @param pos A position, an optional placement rule for the bars, defaults
-    #'   to "dodge"
-    #' @param barplot_alpha A floating number between 0 and 1, an optional 
-    #'   degree of transparency for the bars, default at 1
-    #'
-    #' @param colour_title legend guide for scale_colour, setting a title 
-    #'   activates the legend, requires colour_vars to be set, defaults to
-    #'   FALSE.
-    #' @param fill_title legend guide for scale_fill, setting a title activates
-    #'   the legend, requires fill_vars to be set, default at FALSE.
-    #'
-    #' @param legend_labels a vector of labels for the legend, e.g, 
-    #'   c("M\\u0101ori", "Pakeh\\u0101")
-    #' @param legend_rev determines whether the order of the legend labels
-    #'   should be reversed
-    #' @param flip Flips x-axis and y-axis if TRUE, defaults to FALSE
-    #'
-    #' @return a bar plot object
-    #' @examples
-    #'
-    #' \dontrun{
-    #' library(tidyverse)
-    #' iris.plot <- iris %>% group_by(Species) %>% summarize(Sepal.Length=mean(Sepal.Length))
-    #'
-    #'   #Simple bar plot
-    #'   p <- plot_barplot(iris.plot,
-    #'                     x_var = "Species",
-    #'                     y_var = "Sepal.Length")
-    #'   plot(p)
-    #'
-    #'   #Same bar plot with fill by Species and border colour by Species reversed
-    #'   p <- plot_barplot(iris.plot,
-    #'                     main_title="Sepal.Length by Species on the Iris Data Set",
-    #'                     x_var = "Species",
-    #'                     y_var = "Sepal.Length",
-    #'                     fill_title = "Species",
-    #'                     fill_vars = "Species",
-    #'                     colour_vars = "rev(Species)")
-    #'   plot(p)
-    #'
-    #'   #Same bar plot with a facet for Sepal.Width and a legend with a title
-    #'   p <- plot_barplot(iris,
-    #'                     x_var = "Species",
-    #'                     y_var = "Sepal.Length",
-    #'                     fill_vars = "Species",
-    #'                     y_facet = "Sepal.Width ~ .",
-    #'                     fill_title = "Species",
-    #'                     flip = TRUE)
-    #'   plot(p)}
-    #'
-    #' @section Bugs:
-    #' Guide colour and guide fill currently take the same arguments and can
-    #' not be controlled individually.
-    #'
-    #' @export
-    set_bar_theme()
 
     p <- ggplot2::ggplot(data = data_frame)
 
@@ -215,8 +216,8 @@ plot_barplot <-
       p <-
         p + ggplot2::geom_label(
                        ggplot2::aes_string(y = paste(y_var, " - 0.00"), fill = rev(fill_vars)),
-                       colour = ggplot2::theme_get()[["bar.label.color"]],
-                       size = ggplot2::theme_get()[["bar.label.size"]],
+                       colour = get_bar_theme()[["bar.label.color"]],
+                       size = get_bar_theme()[["bar.label.size"]],
                        show.legend = FALSE,
                        position = pos
                      )
@@ -232,7 +233,7 @@ plot_barplot <-
     if (!is.null(main_title)) {
       cow <- cowplot::ggdraw()
       cow <-
-        cow + cowplot::draw_label(main_title, size = ggplot2::theme_get()[["main.title.size"]])
+        cow + cowplot::draw_label(main_title, size = get_bar_theme()[["main.title.size"]])
       p <-
         cowplot::plot_grid(cow, p, ncol = 1, rel_heights = c(0.1, 1))
     }
--- R/set_bar_theme.R
+++ R/set_bar_theme.R
@@ -1,35 +1,114 @@
-set_bar_theme <- function(text_col = black_seq[8], basesize = 12, legend_col = "#E5E2E0", legend_pos = "bottom", keysize = 0.7) {
-  #' Sets a theme for a barplot
-  #' @param text_col Text colour, defaults to black_seq[8]
-  #' @param basesize Text basesize, defaults to 12
-  #' @param legend_col legend background colour, default at "#E5E2E0"
-  #' @param legend_pos legend positions, takes a string ("top", "bottom", 
-  #'   "right", "left"), or a vector of two numbers between 0 and 1, indicating 
-  #'   the relative x and y positions.
-  #' @param keysize size of the legend
-  #' @export
-  bar_theme <- ggplot2::theme_bw() +
-    ggplot2::theme(legend.text = ggplot2::element_text(size = basesize - 2, colour = text_col),
-                   legend.title = ggplot2::element_text(size = basesize, colour = text_col),
-                   legend.key.size = ggplot2::unit(keysize, "cm"),
-                   legend.key = ggplot2::element_blank(),
-                   legend.background = ggplot2::element_rect(colour = legend_col),
-                   legend.position = legend_pos,
-                   axis.title.x = ggplot2::element_text(vjust = 3.1804, colour = text_col, size = basesize + 2, margin=ggplot2::margin(25,0,0,0)),
-                   axis.text.x = ggplot2::element_text(size = basesize, lineheight = 1.2, vjust = 1),
-                   axis.title.y = ggplot2::element_text(vjust = 0.6316, colour = text_col, size = basesize + 2),
-                   axis.text.y = ggplot2::element_text(size = basesize-2, colour = text_col, lineheight = 1.2, vjust = 0.5),
-                   axis.ticks.length = ggplot2::unit(.10888, "cm"),
-                   ## plot.title = ggplot2::element_text(vjust = 3.8746, hjust = 0, colour = text_col, size = basesize + 8),
-                   plot.margin = ggplot2::unit(c(.8835, 0, 0.0748, 0), "cm"),
-                   panel.border = ggplot2::element_rect(color = "grey", fill = NA, size = 1), ## for facet borders
-                   strip.background = ggplot2::element_rect(fill = "#ececec", colour = black_seq[8]), # facet background
-                   strip.text.x = ggplot2::element_text(size = basesize), # for facet text
-                   strip.text.y = ggplot2::element_text(size = basesize, angle = 0)) # for facet text
+theme_env <- new.env(parent=emptyenv())
 
-  bar_theme["main.title.size"] <- basesize+5
-  bar_theme["bar.label.size"] <- basesize-9
-  bar_theme["bar.label.color"] <- text_col
+#' Sets a theme for a barplot
+#'
+#' @param text_col Text colour, defaults to black_seq[8]
+#' @param basesize Text basesize, defaults to 12
+#' @param legend_col legend background colour, default at "#E5E2E0"
+#' @param legend_pos legend positions, takes a string ("top", "bottom", 
+#'   "right", "left"), or a vector of two numbers between 0 and 1, indicating 
+#'   the relative x and y positions.
+#' @param keysize size of the legend
+#' @export
+set_bar_theme <-
+  function(text_col="#000000",
+           basesize=12,
+           legend_col="#E5E2E0",
+           legend_pos="bottom",
+           keysize=0.7){
 
-  ggplot2::theme_set(bar_theme)
+    bar_theme <- ggplot2::theme_bw()
+
+    ## R colour codes
+    ## http://colorbrewer2.org/
+    bar_theme[["black_seq"]] <-
+      list('#ffffff', '#f0f0f0', '#d9d9d9',
+           '#bdbdbd', '#969696', '#737373',
+           '#525252', '#252525', '#000000')
+
+    bar_theme[["blue_seq"]] <-
+      list('#f7fbff', '#deebf7', '#c6dbef',
+           '#9ecae1', '#6baed6', '#4292c6',
+           '#2171b5', '#08519c', '#08306b')
+
+    bar_theme[["green_seq"]] <-
+      list('#f7fcf5', '#e5f5e0', '#c7e9c0',
+           '#a1d99b', '#74c476', '#41ab5d',
+           '#238b45', '#006d2c', '#00441b')
+
+    bar_theme[["orange_seq"]] <-
+      list('#fff5eb', '#fee6ce', '#fdd0a2',
+           '#fdae6b', '#fd8d3c', '#f16913',
+           '#d94801','#a63603','#7f2704')
+
+    bar_theme[["purple_seq"]] <-
+      list('#fcfbfd', '#efedf5', '#dadaeb',
+           '#bcbddc', '#9e9ac8', '#807dba',
+           '#6a51a3', '#54278f', '#3f007d')
+
+    bar_theme[["red_seq"]] <-
+      list('#fff5f0', '#fee0d2', '#fcbba1',
+           '#fc9272', '#fb6a4a', '#ef3b2c',
+           '#cb181d', '#a50f15', '#67000d')
+
+    bar_theme <- bar_theme +
+      ggplot2::theme(
+                 ## legent_settings
+                 legend.text=ggplot2::element_text(
+                                        size=basesize - 2,
+                                        colour=text_col),
+                 legend.title=ggplot2::element_text(
+                                         size=basesize,
+                                         colour=text_col),
+                 legend.key.size=ggplot2::unit(keysize, "cm"),
+                 legend.key=ggplot2::element_blank(),
+                 legend.background=ggplot2::element_rect(
+                                              colour=legend_col),
+                 legend.position=legend_pos,
+                 ## axis settings
+                 axis.title.x=ggplot2::element_text(
+                                         vjust=3.1804,
+                                         colour=text_col,
+                                         size=basesize + 2,
+                                         margin=ggplot2::margin(25,0,0,0)),
+                 axis.text.x=ggplot2::element_text(
+                                        size=basesize,
+                                        lineheight=1.2,
+                                        vjust=1),
+                 axis.title.y=ggplot2::element_text(
+                                         vjust=0.6316,
+                                         colour=text_col,
+                                         size=basesize + 2),
+                 axis.text.y=ggplot2::element_text(
+                                        size=basesize-2,
+                                        colour=text_col,
+                                        lineheight=1.2,
+                                        vjust=0.5),
+                 axis.ticks.length=ggplot2::unit(.10888, "cm"),
+
+
+                 plot.margin=ggplot2::unit(c(.8835, 0, 0.0748, 0), "cm"),
+                 panel.border=ggplot2::element_rect(
+                                         color="grey",
+                                         fill=NA,
+                                         size=1), ## for facet borders
+                 strip.background=ggplot2::element_rect(
+                                             fill="#ececec",
+                                             colour=bar_theme[["black_seq"]][[8]]), # facet background
+                 strip.text.x=ggplot2::element_text(
+                                         size=basesize), # for facet text
+                 strip.text.y=ggplot2::element_text(
+                                         size=basesize,
+                                         angle=0)) # for facet text
+
+    bar_theme["main.title.size"] <- basesize+5
+    bar_theme["bar.label.size"] <- basesize-9
+    bar_theme["bar.label.color"] <- text_col
+
+    theme_env$bar_theme <- bar_theme
+    ggplot2::theme_set(theme_env$bar_theme)
+  }
+
+get_bar_theme <- function(){
+  return(theme_env$bar_theme)
 }
