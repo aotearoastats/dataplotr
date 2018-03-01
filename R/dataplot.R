f1f475f26df31a1f7aa58a22bddb12c10a093e83
Alexander Kegel <afk@mailbox.org>
2018-03-02 12:56:13 +1300

    Refactor for modularity

---
+++ R/dataplot.R
@@ -0,0 +1,47 @@
+#' base function to build build up graphs element by element
+dataplot <-
+  function(data_frame,
+           x_var,
+           y_var,
+           element_function,
+           main_title=NULL,
+           bar_width = NULL,
+           x_title = ggplot2::waiver(),
+           x_ticks = TRUE,
+           y_title = ggplot2::waiver(),
+           x_limits = NULL,
+           y_limits = NULL,
+           x_breaks = ggplot2::waiver(),
+           y_breaks = ggplot2::waiver(),
+           y_facet = FALSE,
+           label_vars = FALSE,
+           percentage = FALSE,
+           colour_vars = NULL,
+           fill_vars = NULL,
+           colour_palette = NULL,
+           fill_palette = NULL,
+           pos = "dodge",
+           barplot_alpha = 1,
+           colour_title = FALSE,
+           fill_title = FALSE,
+           legend_labels = ggplot2::waiver(),
+           legend_rev = FALSE,
+           flip = FALSE){
+
+    p <- ggplot2::ggplot(data = data_frame)
+
+    p <- p + ggplot2::aes_string(x = x_var,
+                                 y = y_var,
+                                 fill = fill_vars,
+                                 colour = colour_vars,
+                                 label = label_vars
+                                 )
+
+    e <- environment()
+    for(f in element_function) {
+      environment(f) <- e
+      p <- f(p)
+    }
+
+    return(p)
+  }
--- R/plot_barplot.R
+++ R/plot_barplot.R
@@ -69,7 +69,7 @@
 #'   p <- plot_barplot(iris.plot,
 #'                     x_var = "Species",
 #'                     y_var = "Sepal.Length")
-#'   plot(p)
+#'   print(p)
 #'
 #'   #Same bar plot with fill by Species and border colour by Species reversed
 #'   p <- plot_barplot(iris.plot,
@@ -79,7 +79,7 @@
 #'                     fill_title = "Species",
 #'                     fill_vars = "Species",
 #'                     colour_vars = "rev(Species)")
-#'   plot(p)
+#'   print(p)
 #'
 #'   #Same bar plot with a facet for Sepal.Width and a legend with a title
 #'   p <- plot_barplot(iris,
@@ -89,154 +89,23 @@
 #'                     y_facet = "Sepal.Width ~ .",
 #'                     fill_title = "Species",
 #'                     flip = TRUE)
-#'   plot(p)}
+#'   print(p)}
 #'
 #' @section Bugs:
 #' Guide colour and guide fill currently take the same arguments and can
 #' not be controlled individually.
 #'
 #' @export
-plot_barplot <-
-  function(data_frame,
-           x_var,
-           y_var,
-           main_title = NULL,
-           bar_width = NULL,
-           x_title = ggplot2::waiver(),
-           x_ticks = TRUE,
-           y_title = ggplot2::waiver(),
-           x_limits = NULL,
-           y_limits = NULL,
-           x_breaks = ggplot2::waiver(),
-           y_breaks = ggplot2::waiver(),
-           y_facet = FALSE,
-           label_vars = FALSE,
-           percentage = FALSE,
-           colour_vars = NULL,
-           fill_vars = NULL,
-           colour_palette = NULL,
-           fill_palette = NULL,
-           pos = "dodge",
-           barplot_alpha = 1,
-           colour_title = FALSE,
-           fill_title = FALSE,
-           legend_labels = ggplot2::waiver(),
-           legend_rev = FALSE,
-           flip = FALSE){
-
-    p <- ggplot2::ggplot(data = data_frame)
-
-    p <- p + ggplot2::aes_string(
-                        x = x_var,
-                        y = y_var,
-                        fill = fill_vars,
-                        colour = colour_vars,
-                        label = label_vars
-                      )
-
-    p <- p + ggplot2::geom_bar(
-                        position = pos,
-                        alpha = barplot_alpha,
-                        stat = "identity",
-                        width = bar_width,
-                        size = 1
-                      ) ## controls border thickness
-
-    p <- p + ggplot2::scale_x_discrete(
-                        x_title,
-                        drop = FALSE,
-                        limits = rev(x_limits),
-                        breaks = x_breaks
-                      )
-
-
-    ## percentage for y scale
-    if (percentage == TRUE) {
-      p <- p + ggplot2::scale_y_continuous(
-                          y_title,
-                          labels = scales::percent,
-                          limits = y_limits,
-                          breaks = y_breaks
-                        )
-    } else {
-      p <- p + ggplot2::scale_y_continuous(
-                          y_title,
-                          limits = y_limits,
-                          breaks = y_breaks)
-    }
-
-    ## colour either manual or brewer
-    if (!(is.null(colour_palette)) & !(is.null(fill_palette))) {
-      p <- p + ggplot2::scale_colour_manual(
-                          values = colour_palette,
-                          labels = legend_labels)
-      p <- p + ggplot2::scale_fill_manual(
-                          values = fill_palette,
-                          labels = legend_labels)
-    } else {
-      p <- p + ggplot2::scale_colour_brewer(
-                          labels = legend_labels,
-                          direction = -1)
-      p <- p + ggplot2::scale_fill_brewer(
-                          labels = legend_labels,
-                          direction = -1)
-    }
-
-    ## facet
-    if (!(y_facet == FALSE)) {
-      p <-
-        p + ggplot2::facet_grid(y_facet, drop = TRUE, scales = "free_x")
-    }
-
-    ## legend
-    p <- p + ggplot2::guides(colour = "none",
-                             fill = "none")
-
-    if (!(colour_title == FALSE)) {
-      p <-
-        p + ggplot2::guides(colour = ggplot2::guide_legend(
-                                                title = colour_title,
-                                                reverse = legend_rev))
-    }
-
-    if (!(fill_title == FALSE)) {
-      p <-
-        p + ggplot2::guides(fill = ggplot2::guide_legend(
-                                              title = fill_title,
-                                              reverse = legend_rev))
-    }
-
-    ## flip axes
-    if (flip) {
-      p <- p + ggplot2::coord_flip()
-    }
-
-    ## bar labels
-    if (!(label_vars == FALSE)) {
-      p <-
-        p + ggplot2::geom_label(
-                       ggplot2::aes_string(y = paste(y_var, " - 0.00"), fill = rev(fill_vars)),
-                       colour = get_bar_theme()[["bar.label.color"]],
-                       size = get_bar_theme()[["bar.label.size"]],
-                       show.legend = FALSE,
-                       position = pos
-                     )
-    }
-
-    ## x_ticks switches off labels and ticks on the x-axis
-    if (!x_ticks) {
-      p <- p + ggplot2::theme(axis.text.x = ggplot2::element_blank(),
-                              axis.ticks.x = ggplot2::element_blank())
-    }
-
-    ## title
-    if (!is.null(main_title)) {
-      cow <- cowplot::ggdraw()
-      cow <-
-        cow + cowplot::draw_label(main_title, size = get_bar_theme()[["main.title.size"]])
-      p <-
-        cowplot::plot_grid(cow, p, ncol = 1, rel_heights = c(0.1, 1))
-    }
-
-    return(p)
-  }
+plot_barplot <- function(...) {
+  set_bar_theme()
+  elements <- list(bar,
+                   scales,
+                   colour,
+                   legend,
+                   flip_axes,
+                   bar_labels,
+                   x_ticks,
+                   title)
+  d <- dataplot(..., element_function=elements)
+  return(d)
+}
